4983fba32d3558d63cd489235c559c62030bb8e5
Gustavo Mendes <gustavormendess@gmail.com>
2024-01-24 23:18:09 -0400

    Add files via upload

---
+++ exerciseTS-8.1/index.ts
@@ -0,0 +1,20 @@
+let names : readonly string[] =  ['Gustavo','Felipe','Camilo'];
+let obj = {
+  name:'',
+  age: 0
+}
+
+function showObj(name:string,age:number){
+
+}
+
+class Dog {
+  name: string
+  dangerous: boolean
+  age: number
+  constructor(name:string,dangerous:boolean,age:number){
+    this.name = name
+    this.dangerous = dangerous
+    this.age = age
+  }
+}
